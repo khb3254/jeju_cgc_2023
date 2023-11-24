b2759d38750d8e478e2abfdd8b5221446319c3b5
khb3254 <khb3254@naver.com>
2023-11-24 09:13:54 +0900

    강나영_최종본
    강나영_태블로 연결본 최종본입니다.

--- jeju/urls.py
+++ jeju/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("AppOfJeju.urls")),
+    path("192.168.108.36", include("AppOfJeju.urls"))
 ]
 
